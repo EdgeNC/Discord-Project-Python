2690340b400878c3530d5a8b2ec9ce80b9113b96
Edge <88197449+EdgeNC@users.noreply.github.com>
2021-10-07 17:42:10 +0700

    Add files via upload

---
+++ discord/bot.py
@@ -0,0 +1,42 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('ODc3MTUwNjQzMDUwMTM5NjY5.YRucMQ.yWgJmyxXUeR_rCfTBORCOLbn32A')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    hello_test = [
+       'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Thanks For using my Bot, '
+            'Welcome To My Discord Server'
+        ),
+    ]
+
+    if message.content == 'doc':
+        response = random.choice(hello_test)
+        await message.channel.send(response)
+    
+
+client.run('ODc3MTUwNjQzMDUwMTM5NjY5.YRucMQ.yWgJmyxXUeR_rCfTBORCOLbn32A')
